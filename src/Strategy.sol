72e9192c8bd7a5ddbe1094aaa5aa22331ef57df5
Steve Pham <33653442+umphams@users.noreply.github.com>
2023-02-05 12:25:10 -0600

    Uncomment clone fn & confirm resolved PR #6 changes

--- src/Strategy.sol
+++ src/Strategy.sol
@@ -6,9 +6,9 @@ import {BaseStrategy, StrategyParams} from "@yearnvaults/contracts/BaseStrategy.
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IGearboxRootVault} from "./interfaces/Mellow/IGearboxRootVault.sol";
+import {IGearboxRootVault} from "./interfaces/Mellow/IGearboxRootVault.sol"; // specific GearboxRootVault for Fearless Gearbox Strategies
 
-/// @title StrategyMellow-GearboxWETH
+/// @title StrategyMellow-Gearbox_wETH
 /// @notice Yearn strategy deploying wETH to Mellow Fearless Gearbox wETH strategy
 /// @author @steve0xp && @0xValJohn
 /// @dev NOTE - contract is a wip still. See PR comments && open issues, && TODOs in this file
@@ -16,13 +16,13 @@ contract Strategy is BaseStrategy {
     using SafeERC20 for IERC20;
     using Address for address;
 
-    IGearboxRootVault public gearboxRootVault; // 0xD3442BA55108d33FA1EB3F1a3C0876F892B01c44 - specific mellowLPT for wETH Fearless Gearbox
+    IGearboxRootVault public gearboxRootVault;
     IERC20 public mellowLPT;
     bool internal isOriginal = true;
 
     /// EVENTS
 
-    // event Cloned(address indexed clone);
+    event Cloned(address indexed clone);
 
     /// GETTERS
 
@@ -35,70 +35,72 @@ contract Strategy is BaseStrategy {
     /// TODO - does `mellowStrategy.tvl()` include rewards converted to wantToken or not?
     /// @dev Question - strategy should only have wantTokens and mellowLPTs, no other ERC20s from the strategy. Those strategies should have been converted
     function estimatedTotalAssets() public view override returns (uint256) {
-        return balanceOfWant() + valueOfMellowLPT(); // TODO - may have to update helper functions to use gearboxRootVault.epochToPriceForLpTokenD18() for the price conversion.
-
-        // return balanceOfWant() + ((mellowLPT.balanceOf(address(this)) * 1e18 / mellowLPT.totalSupply()) * mellowStrategy.tvl()) / 1e18;   // TODO - Delete if deemed irrelevant; this was the old no-helper function way
+        return balanceOfWant() + valueOfMellowLPT(); // TODO - may have to update helper functions to use gearboxRootVault.epochToPriceForLpTokenD18() for the price conversion. See issue #15
     }
 
     /// CONSTRUCTOR
 
-    /// @notice setup w/ wETH vault, baseStrategy && mellow Strategy
-    /// @param _vault yearn v2 vault allocating collateral to this strategy
-    /// @param _mellowRootVault yearn v2 vault allocating collateral to this strategy
+    /// @notice setup w/ wETH vault, baseStrategy && Yearn Mellow Strategy
+    /// @param _vault Yearn v2 vault allocating collateral to this strategy
+    /// @param _mellowRootVault // specific root vault for Mellow-Gearbox strategies, specific to wantToken. ex.) wETH: 0xD3442BA55108d33FA1EB3F1a3C0876F892B01c44
     constructor(address _vault, address _mellowRootVault) public BaseStrategy(_vault) {
         _initializeStrategy(__mellowRootVault);
     }
 
     /// SETTERS
 
-    /// @notice initialize strategy clones for new assets.
+    /// @notice initialize Yearn-Mellow-Gearbox strategy clones for new wantTokens
     function initialize(
         address _vault,
         address _strategist,
         address _rewards,
         address _keeper,
         address _mellowRootVault
-    ) public {
+    ) public override {
         require(address(gearboxRootVault) == address(0)); // @note Only initialize once
 
         _initialize(_vault, _strategist, _rewards, _keeper);
         _initializeStrategy(_mellowRootVault);
     }
 
-    // /// @notice create and initialize new Yearn-Mellow strategy for different asset
-    // function clone(address _vault, address _strategist, address _rewards, address _keeper, address _mellowRootVault)
-    //     external
-    //     returns (address payable newStrategy)
-    // {
-    //     require(isOriginal); //??
+    /// @notice create and initialize new Yearn-Mellow-Gearbox strategy for different wantToken
+    function clone(address _vault, address _strategist, address _rewards, address _keeper, address _mellowRootVault)
+        external
+        override
+        returns (address payable newStrategy)
+    {
+        require(isOriginal);
 
-    //     bytes20 addressBytes = bytes20(address(this));
+        bytes20 addressBytes = bytes20(address(this));
 
-    //     // TODO - understand this more
-    //     assembly {
-    //         let clone_code := mload(0x40)
-    //         mstore(clone_code, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-    //         mstore(add(clone_code, 0x14), addressBytes)
-    //         mstore(add(clone_code, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-    //         newStrategy := create(0, clone_code, 0x37)
-    //     }
+        assembly {
+            let clone_code := mload(0x40)
+            mstore(clone_code, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(add(clone_code, 0x14), addressBytes)
+            mstore(add(clone_code, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            newStrategy := create(0, clone_code, 0x37)
+        }
 
-    //     Strategy(newStrategy).initialize(_vault, _strategist, _rewards, _keeper, _mellowRootVault);
+        Strategy(newStrategy).initialize(_vault, _strategist, _rewards, _keeper, _mellowRootVault);
 
-    //     emit Cloned(newStrategy);
-    // }
+        emit Cloned(newStrategy);
+    }
+
+    /// @notice permissioned manual withdrawal from Yearn-Mellow-Gearbox strategy
+    /// @dev TODO - coordinate this w/ harvest sequece: do we have two manual functions: `manualRegisterWithdraw()` & `manualWithdraw()`?
+    function manualWithdraw() external onlyVaultManagers {}
 
     /// INTERNAL FUNCTIONS
 
     /// @notice initialize Yearn strategy by setting Mellow root vault
     function _initializeStrategy(address _mellowRootVault) internal {
-        gearboxRootVault = IGearboxRootVault(_mellowRootVault); // specific GearboxRootVault for Fearless Gearbox Strategies
+        gearboxRootVault = IGearboxRootVault(_mellowRootVault);
         mellowLPT = IERC20(_mellowRootVault);
     }
 
-    /// @notice called when preparing return to have proper accounting of losses and gains from the last time harvest() has been called.
-    /// @param debtOutstanding - how much want token does the vault want right now. You're preparing the return of the wantToken, liquidiating anything you can to get that amount back to the vault.
-    /// @dev Part of Harvest "flow" - bot calls "harvest()", it calls this function && adjustPosition()
+    /// @notice called when preparing return to have accounting of losses & gains from the last harvest(), and liquidates positions if rqd
+    /// @param debtOutstanding how much wantToken the vault requests
+    /// @dev Part of Harvest 'workflow' - bot calls `harvest()`, it calls this function && `adjustPosition()`
     /// Question - can _debtOutstanding ever be bigger than what the vault actually allocated to the strategy? I assume this is defined in the vault logic / workflows
     function prepareReturn(uint256 _debtOutstanding)
         internal
@@ -112,7 +114,6 @@ contract Strategy is BaseStrategy {
         uint256 _totalDebt = vault.strategies(address(this)).totalDebt;
 
         if (_totalAssets >= _totalDebt) {
-            // Implicitly, _profit & _loss are 0 before we change them.
             _profit = _totalAssets - _totalDebt;
         } else {
             _loss = _totalDebt - _totalAssets;
@@ -126,7 +127,6 @@ contract Strategy is BaseStrategy {
 
         _debtPayment = Math.min(_debtOutstanding, _amountFreed);
 
-        // TODO - get referesher on this underflow math
         if (_loss > _profit) {
             _loss = _loss - _profit;
             _profit = 0;
@@ -137,7 +137,7 @@ contract Strategy is BaseStrategy {
     }
 
     /// @notice investing excess want token into the strategy
-    /// @dev Part of Harvest "flow" - bot calls "harvest()", it calls this function && prepareReturn()
+    /// @dev Part of Harvest 'workflow' - bot calls `harvest()`, it calls this function && `prepareReturn()`
     /// @param _debtOutstanding amount of debt from Vault required at minimum
     /// @dev TODO - if we are claiming rewards that are still in CRV and CVX from the strategy then we'll have to swap them. Right now it seems that Mellow Protocol will have them ready in wantToken. TBD from convos.
     function adjustPosition(uint256 _debtOutstanding) internal override {
@@ -159,7 +159,7 @@ contract Strategy is BaseStrategy {
 
         // Invest the rest of the want
         uint256 _excessWETH = _WETHBal - _debtOutstanding;
-        uint256 lptMinimum = 0; // TODO: do we want to define a minimum LP amount to receive based on the balance in the Mellow vault system?
+        uint256 lptMinimum = 0; // TODO - add minimum LP amount to receive
 
         uint256 lpAmount = gearboxRootVault.deposit(_excessWETH, lptMinimum, ""); // TODO: What should we do with the vaultOptions param --> I see it in one of their test files for claimToken as "bytes[] memory vaultOptions = new bytes[](2);"
 
@@ -168,7 +168,7 @@ contract Strategy is BaseStrategy {
 
     /// @notice Liquidate up to `_amountNeeded` of `want` of this strategy's positions, irregardless of slippage. Any excess will be re-invested with `adjustPosition()`.
     /// @param _amountNeeded amount of `want` tokens needed from external call
-    /// @return _liquidatedAmount amount of `want` tokens made available by the liquidation.
+    /// @return _liquidatedAmount amount of `want` tokens made available by the liquidation
     /// @return _loss indicates whether the difference is due to a realized loss, or if there is some other sitution at play (e.g. locked funds) where the amount made available is less than what is needed.
     /// @dev NOTE: The invariant `_liquidatedAmount + _loss <= _amountNeeded` should always be maintained
     function liquidatePosition(uint256 _amountNeeded)
@@ -199,11 +199,11 @@ contract Strategy is BaseStrategy {
 
     /// @notice withdraw specified amount of want from strategy
     /// @param _amount needed to be withdrawn from underlying protocol
-    /// @dev TODO - decide on how yearn is checking balance of wantTokens inside of strategy. 1. Use Mellow `tvl()` function for vaults, 2. query Gearbox for Mellow vault address and do the math based on yearn's LPT ratio vs rest of amount in Geearbox for Mellow. (1st is easier, 2nd is more to the source)
+    /// @dev TODO - decide on how Yearn is checking balance of wantTokens inside of strategy. 1. Use Mellow `tvl()` function for vaults, 2. query Gearbox for Mellow vault address and do the math based on Yearn's LPT ratio vs rest of amount in Geearbox for Mellow. (1st is easier, 2nd is more to the source)
     function _withdrawSome(uint256 _amount) internal {
         uint256 _lptToBurn = Math.min(wantToMelloToken(_amount), balanceOfMellowToken()); // see dev comment above
 
-        gearboxRootVault.registerWithdrawal(_lptToBurn); // queues up withdrawals for current epoch. Also closes out any hanging withdrawals from before, so may have more wantToken in the strategy then we wanted from this.
+        gearboxRootVault.registerWithdrawal(_lptToBurn); // NOTE - queues up withdrawals for current epoch. Also closes out any hanging withdrawals from before, so may have more wantToken in the strategy then we wanted from this.
 
         // TODO: write up an assertion to ensure that redemption was successfully transacted.
     }
@@ -221,20 +221,21 @@ contract Strategy is BaseStrategy {
     // solhint-disable-next-line no-empty-blocks
     {}
 
+    /// @inheritdoc BaseStrategy
     function ethToWant(uint256 _amtInWei) public view virtual override returns (uint256) {
         return _amtInWei;
     }
 
     /// KEEP3RS
 
-    // use this to determine when to harvest
+    /// @inheritdoc BaseStrategy
     function harvestTrigger(uint256 callCostinEth) public view override returns (bool) {
         StrategyParams memory params = vault.strategies(address(this));
         return super.harvestTrigger(callCostInWei) || block.timestamp - params.lastReport > minReportDelay;
     }
 
-    // check if the current baseFee is below our external target
-    // TODO: not sure if this address is the one to use still
+    /// @inheritdoc BaseStrategy
+    /// TODO: not sure if this address is the one to use still
     function isBaseFeeAcceptable() internal view returns (bool) {
         return IBaseFee(0xb5e1CAcB567d98faaDB60a1fD4820720141f064F).isCurrentBaseFeeAcceptable();
     }
